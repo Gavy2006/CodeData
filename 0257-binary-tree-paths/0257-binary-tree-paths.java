071331d18ef3c606fb47dcd3f523348899d3c22c
Gavy <gavygaba13@gmail.com>
2026-07-31 11:10:54 +0530

    Time: 4 ms (72.97%), Space: 50.1 MB (10.22%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
+        
+        List<String> list = new ArrayList<>() ;
 
-        List<String> list = new ArrayList<>();
-
-        String str = String.valueOf(root.val);
-
-        return mycode(list, str, root);
+        return mycode(list , root , String.valueOf(root.val));
     }
 
-    public static List<String> mycode(List<String> list, String str, TreeNode node) {
+    public static List<String> mycode(List<String> list , TreeNode node , String str){
 
-        if (node.left == null && node.right == null) {
-            list.add(str);
-            return list;
+        if(node.left == null && node.right == null){
+           list.add(str);
+           return list ;
         }
 
-        if (node.right != null) {
-            mycode(list, str + "->" + node.right.val, node.right);
-        }
+      if(node.left != null){
+        mycode(list , node.left , str + "->" + + node.left.val ) ;
+      }
 
-        if (node.left != null) {
-            mycode(list, str + "->" + node.left.val, node.left);
-        }
-    
-        return list;
+     if(node.right != null){
+        mycode(list , node.right , str + "->" + node.right.val  ) ;
+      }
+
+
+        return list ;
     }
 }
