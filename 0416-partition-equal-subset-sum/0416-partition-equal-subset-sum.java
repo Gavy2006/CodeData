6d0718fa7bd2392b31fd1d9d4e198c6138b5af3e
Gavy <gavygaba13@gmail.com>
2026-08-01 10:29:11 +0530

    Time: 98 ms (17.48%), Space: 75.1 MB (21.43%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,34 +1,39 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        
-        int target = 0 ;
 
-        for(int no : nums){
-            target+=no ;
+        int target = 0;
+
+        for (int no : nums) {
+            target += no;
         }
 
-        if(target%2 != 0)  return false ;
+        if (target % 2 != 0)
+            return false;
 
-        Boolean[][] dp = new Boolean[nums.length][target/2 + 1] ;
-        return mycode(nums , target , 0 , 0  , dp) ;
+        target /= 2;
 
+        Boolean[][] dp = new Boolean[nums.length][target + 1];
+
+        return mycode(nums, target, 0, 0, dp);
     }
 
-    public static boolean mycode(int[] nums , int target , int sum , int index , Boolean[][] dp){
+    public static boolean mycode(int[] nums, int target, int sum, int index, Boolean[][] dp) {
 
-       if(target - 2*sum == 0) return true ;
+        if (sum == target)
+            return true;
 
-       if(sum > target/2) return false ;
-        
-       if(index == nums.length) return false ;
+        if (sum > target)
+            return false;
 
-       if(dp[index][sum] != null) return dp[index][sum] ;
+        if (index == nums.length)
+            return false;
 
-       boolean a = mycode(nums , target , sum + nums[index] , index +1 , dp) ;
-       boolean b = mycode(nums , target , sum  , index +1 , dp) ;
+        if (dp[index][sum] != null)
+            return dp[index][sum];
 
-       return dp[index][sum] = a || b ;
+        boolean take = mycode(nums, target, sum + nums[index], index + 1, dp);
+        boolean notTake = mycode(nums, target, sum, index + 1, dp);
 
-
+        return dp[index][sum] = take || notTake;
     }
 }
