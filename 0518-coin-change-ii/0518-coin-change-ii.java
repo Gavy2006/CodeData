0e2fc0299c3f89899ca17f347fdcc7d8f029c0b4
Gavy <gavygaba13@gmail.com>
2026-08-01 10:59:06 +0530

    Time: 36 ms (6.64%), Space: 54.6 MB (59.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,29 +1,25 @@
 class Solution {
     public int change(int amount, int[] coins) {
-     
-
+        
         Integer[][] dp = new Integer[coins.length][amount+1] ;
 
-        return mycode(coins , dp , 0 , 0 , amount) ;
+        return mycode(coins , dp , 0 , 0 , amount ) ;
     }
 
-    public static int mycode(int[] coins , Integer[][] dp , int index , int sum , int target){
-
+    public static int mycode( int[] arr , Integer[][] dp , int index , int sum , int target){
+       
         if(sum == target) return 1 ;
 
         if(sum > target) return 0 ;
 
-        if(index == coins.length) return 0 ;
+        if(index == arr.length) return 0 ;
 
         if(dp[index][sum] != null) return dp[index][sum] ;
+        
+        int a =   mycode( arr , dp , index , sum + arr[index] , target ) ;
+        int b =   mycode(arr , dp , index + 1, sum , target ) ;
 
-  
-    int a = mycode(coins , dp , index  , sum+coins[index]  , target) ;
-    int b = mycode(coins , dp , index + 1 , sum  , target) ;
+      return   dp[index][sum] = a + b ; 
 
-
-              dp[index][sum] = a + b ;
-
-        return dp[index][sum] ;
     }
 }
