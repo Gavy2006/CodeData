2d4ad4331fdbd0fa82006acfe0acd84d3ad25db1
Gavy <gavygaba13@gmail.com>
2026-08-01 11:02:13 +0530

    Time: 32 ms (10.68%), Space: 54.5 MB (59.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,25 +1,28 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        
-        Integer[][] dp = new Integer[coins.length][amount+1] ;
 
-        return mycode(coins , dp , 0 , 0 , amount ) ;
+        Integer[][] dp = new Integer[coins.length][amount + 1];
+
+        return mycode(coins, dp, 0, amount);
     }
 
-    public static int mycode( int[] arr , Integer[][] dp , int index , int sum , int target){
-       
-        if(sum == target) return 1 ;
+    public static int mycode(int[] coins, Integer[][] dp, int index, int remaining) {
 
-        if(sum > target) return 0 ;
+        if (remaining == 0)
+            return 1;
 
-        if(index == arr.length) return 0 ;
+        if (remaining < 0)
+            return 0;
 
-        if(dp[index][sum] != null) return dp[index][sum] ;
-        
-        int a =   mycode( arr , dp , index , sum + arr[index] , target ) ;
-        int b =   mycode(arr , dp , index + 1, sum , target ) ;
+        if (index == coins.length)
+            return 0;
 
-      return   dp[index][sum] = a + b ; 
+        if (dp[index][remaining] != null)
+            return dp[index][remaining];
 
+        int take = mycode(coins, dp, index, remaining - coins[index]);
+        int notTake = mycode(coins, dp, index + 1, remaining);
+
+        return dp[index][remaining] = take + notTake;
     }
 }
