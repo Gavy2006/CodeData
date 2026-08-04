07d7de8ef94b2d3dacb9bc94ef33396f98827d17
Gavy <gavygaba13@gmail.com>
2026-08-04 15:27:58 +0530

    Time: 32 ms (9.27%), Space: 62.2 MB (5.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,23 +1,31 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
+    public int longestCommonSubsequence(String text1, String text2) {
         
-        Integer[][] dp = new Integer[s1.length()][s2.length()] ;
-        return mycode(s1, s2, s1.length() - 1, s2.length() - 1 , dp);
+        Integer[][] dp = new Integer[text1.length() ][text2.length() ] ;
+
+        return mycode(text1 , text2 , 0 , 0  , dp) ;
     }
 
-    public static int mycode(String s1 , String s2 , int i1 , int i2 , Integer[][] dp){
+    public static int mycode(String text1 , String text2 , int i1 , int i2 , Integer[][] dp ){
 
-        if(i1 < 0 || i2 < 0) return 0 ;
-      
+        if(i1 == text1.length() || i2 == text2.length()) return 0 ;
 
-      if(dp[i1][i2] != null) return dp[i1][i2] ;
+       if(dp[i1][i2] != null) return dp[i1][i2] ;
 
-        else if (s1.charAt(i1) == s2.charAt(i2)){
-            return dp[i1][i2] =  1 + mycode(s1 , s2 , i1-1 , i2-1 , dp) ;
-        }
 
-        else {
-            return dp[i1][i2] =  0 +  Math.max(mycode(s1 , s2 , i1-1 , i2 , dp)  , mycode(s1 , s2 , i1 , i2-1 , dp) ) ;
-        }
+       if(text1.charAt(i1) == text2.charAt(i2)){
+
+        return dp[i1][i2]  = 1 + mycode(text1 , text2 , i1+1 ,i2 +1 , dp) ;
+       }
+
+
+       else{
+
+        return dp[i1][i2]  = Math.max(mycode(text1 , text2 , i1+1 ,i2  , dp) , mycode(text1 , text2 , i1 ,i2 +1 , dp)) ; 
+       }
+
+
+
+
     }
 }
