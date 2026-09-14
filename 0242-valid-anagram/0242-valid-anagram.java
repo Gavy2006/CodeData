097f90de81912ed939b0f225983f41e8965dee4b
Gavy <gavygaba13@gmail.com>
2026-09-14 17:32:09 +0530

    Time: 3 ms (94.08%), Space: 46.3 MB (41.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        char[] arr = s.toCharArray() ;
+           Arrays.sort(arr) ;
+           s = new String(arr) ;
+
+           arr = t.toCharArray() ;
+           Arrays.sort(arr) ;
+           t = new String(arr) ;
+
+
+
+           return s.equals(t) ;
+
+    }
+}
