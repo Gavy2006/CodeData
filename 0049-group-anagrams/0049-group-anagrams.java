1de5ca47862208e3442d5f00ea7837d4c6024344
Gavy <gavygaba13@gmail.com>
2026-09-14 17:50:33 +0530

    Time: 6 ms (98.98%), Space: 49.4 MB (92.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,35 +1,33 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        HashMap<String , List<String> > map = new HashMap<>() ;
-
+        HashMap<String , List<String>> map = new HashMap<>() ;
+        
+        
         for(int i = 0 ; i<strs.length ; i++){
+          
+          List<String> list1 = new ArrayList<>() ;
 
-            String str = strs[i] ;
+          String str = strs[i] ;
+          char[] arr = str.toCharArray() ;
+          Arrays.sort(arr) ;
+          str = new String(arr) ;
 
-            char[] arr = str.toCharArray() ;
-             Arrays.sort(arr) ;
-            String str1 = new String(arr) ;
+          if(map.containsKey(str)){
 
-            List<String> list1 = new ArrayList<>() ;
+             list1 = map.get(str) ;
+             list1.add(strs[i]) ;
+             map.put(str , list1) ;
+          }
 
-            if(!map.containsKey(str1)){
-                list1.add(str) ;
-
-                map.put(str1 , list1) ;
-            }
-
-            else{
-
-                list1 = map.get(str1) ;
-                list1.add(str) ;
-                 map.put(str1 , list1) ; ;
-            }
+          else{
+                list1.add(strs[i]) ;
+             map.put(str , list1) ;
+          }
         }
 
-        List<List<String>> list = new ArrayList<>(map.values()) ;
+          List<List<String>> list = new ArrayList<>(map.values()) ;
 
-             
-          return list ;
+        return list ;
     }
 }
