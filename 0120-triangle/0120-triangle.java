6d5fad0af6d997349965e90a957800b63a1c14ca
Gavy <gavygaba13@gmail.com>
2026-09-15 16:32:22 +0530

    Time: 1 ms (99.93%), Space: 46.6 MB (7.74%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,26 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        
-        Integer[][] dp = new Integer[triangle.size()][triangle.size()] ;
-        return mycode(triangle , dp , 0 , 0) ;
+
+        Integer[][] dp = new Integer[triangle.size()][triangle.size()];
+
+        return mycode(triangle, dp, 0, 0);
     }
-  
 
-   public static int mycode(List<List<Integer>> list , Integer[][] dp , int row , int col){
+    public static int mycode(List<List<Integer>> list , Integer[][] dp , int row , int col){
 
-    if(row == list.size()) return 0 ;
- 
-    if(dp[row][col] != null) return dp[row][col] ;
+        if(row == list.size() -1 ) return list.get(row).get(col);
 
-    int a = list.get(row).get(col) + mycode(list , dp , row + 1 , col) ;
-    int b = list.get(row).get(col) + mycode(list , dp , row+1 , col + 1) ;
 
-    return dp[row][col] = Math.min(a , b) ;
+        if(dp[row][col] != null) return dp[row][col] ;
 
-   }
+        int a =  list.get(row).get(col) + mycode(list , dp , row + 1 , col) ;
+
+        int b = list.get(row).get(col) + mycode(list , dp , row + 1 , col + 1) ;
+
+        dp[row][col] = Math.min(a , b) ;
+
+
+
+        return dp[row][col] ;
+    }
 }
