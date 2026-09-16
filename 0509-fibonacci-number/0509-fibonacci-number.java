b0ce4d958f1fd60cf01cf6c57bbf8ba3ba560927
Gavy <gavygaba13@gmail.com>
2026-09-16 11:02:26 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,24 +1,24 @@
 class Solution {
     public int fib(int n) {
+        
+        int[] dp = new int[n+1] ;
 
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp , -1) ;
 
-        return mycode(n, dp);
+        return mycode(dp , n );
     }
 
-    public static int mycode(int n, int[] dp) {
 
-        if (dp[n] != -1)
-            return dp[n];
+    public static int mycode(int[] dp , int n){
 
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        if(n == 0) return 0 ;
+        if(n==1) return 1 ;
 
-        dp[n] = mycode(n - 1 , dp) + mycode(n - 2 , dp);
+        if(dp[n] != -1) return dp[n] ;
 
-        return dp[n];
+
+        return dp[n] = mycode(dp , n-1) + mycode(dp , n-2)  ;
     }
+
+
 }
