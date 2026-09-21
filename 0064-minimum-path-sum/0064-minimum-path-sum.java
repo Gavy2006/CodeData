f38d84d2b516fb66c70bf904810b96a5249d56cc
Gavy <gavygaba13@gmail.com>
2026-09-21 15:01:37 +0530

    Time: 1 ms (99.81%), Space: 50.3 MB (39.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,44 +1,41 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        
+        int row =  grid.length -1 ;
+        int col = grid[0].length-1 ;
 
-        int[][] dp = new int[grid.length][grid[0].length];
+        int[][] dp = new int[grid.length][grid[0].length] ;
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        for(int[] arr : dp){
+    Arrays.fill(arr, -1);
+}
 
-        int row = grid.length - 1;
-        int col = grid[0].length - 1;
-
-        return mycode(row, col, grid, dp);
+        return mycode(row , col , grid , dp) ;
     }
 
-    public static int mycode(int row, int col, int[][] grid, int[][] dp) {
+    public static int mycode(int row , int col , int[][] grid , int[][] dp){
 
-        if (row == 0 && col == 0)
-            return grid[0][0];
+        if(row == 0 && col == 0){
+            return grid[0][0] ;
+        }
+         
 
-        if (dp[row][col] != -1)
-            return dp[row][col];
+         if(dp[row][col] != -1 ) return dp[row][col];
 
-        int a = Integer.MAX_VALUE;
-        int b = Integer.MAX_VALUE;
+        int a = Integer.MAX_VALUE ;
+        int b = Integer.MAX_VALUE ;
 
-        if (row > 0) {
-
-            a = grid[row][col] + mycode(row - 1, col, grid, dp);
-
+        if(row > 0){
+        a =  grid[row][col] + mycode(row -1 , col , grid , dp) ;
         }
 
-        if (col > 0) {
-
-            b = grid[row][col] + mycode(row, col - 1, grid, dp);
-
+         if(col > 0){
+        b = grid[row][col]+ mycode(row  , col -1 , grid , dp) ;
         }
 
-        dp[row][col] = Math.min(a, b);
+        dp[row][col] = Math.min(a , b) ;
 
-        return dp[row][col];
+
+        return dp[row][col]; 
     }
-
 }
