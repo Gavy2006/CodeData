f2d186a431db32b630af2a261905745c941368da
Gavy <gavygaba13@gmail.com>
2026-09-21 14:53:32 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,38 +1,34 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        
-        int[][] dp = new int[m+1][n+1] ;
 
-        for(int[] row : dp){
-            Arrays.fill(row , -1) ;
-        }
+     int[][] dp = new int[m][n] ;
+      
+      for(int[] row : dp){
+        Arrays.fill(row , -1) ;
+      }
 
-        return mycode(m-1 , n-1 , dp) ;
-
+     return mycode(m-1 , n-1 , dp) ;   
     }
 
-    public static int mycode(int m , int n , int[][] dp){
+    public static int mycode(int m , int n , int[][]dp){
 
-      if(m == 0 && n == 0) return 1 ;
+        if(m==0 && n == 0 ) return 1 ;
 
-      if(dp[m][n] != -1) return dp[m][n] ;
+       if(dp[m][n] != -1) return dp[m][n] ;
+        
+        int a = 0 ;
+        int b = 0 ;
 
+        if(m > 0){
+          a = mycode(m-1 , n , dp) ;
+        }
 
-    int a = 0 ;
-    int b = 0 ;
+        if(n > 0){
+          b = mycode(m , n-1 , dp) ;
+        }
 
-     if(m > 0){
-        a = mycode(m -1 , n , dp) ;
-     }
+        dp[m][n] = a+b ;
 
-     if(n > 0){
-        b = mycode(m  , n -1 , dp) ;
-     }
-
-
-     dp[m][n] = a+b ;
-
-
-      return dp[m][n] ;
-          }
+        return dp[m][n] ;
+    }
 }
