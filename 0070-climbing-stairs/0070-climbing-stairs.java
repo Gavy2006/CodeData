bb794b14e00f3fc92a527dcbf253da0544715f5c
Gavy <gavygaba13@gmail.com>
2026-09-21 14:02:59 +0530

    Time: 0 ms (100%), Space: 42 MB (54.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,29 +1,24 @@
 class Solution {
     public int climbStairs(int n) {
+        
+        int[] dp = new int[n+1] ;
+        Arrays.fill(dp , -1) ;
 
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-
-        return mycode(n, dp, 0);
+        return mycode(n , dp) ;
     }
 
-    public static int mycode(int n, int[] dp, int index) {
+    public static int mycode(int n , int[] dp){
 
-        if (index > n)
-            return 0;
+        if(n == 0) return 1 ;
+        if(n == 1) return 1 ;
 
-        if (index == n)
-            return 1;
+        if(dp[n] != -1) return dp[n] ;
 
-        if (dp[index] != -1)
-            return dp[index];
+        int a = mycode(n - 1 , dp) ;
+        int b = mycode(n - 2 , dp) ;
 
-        int a = mycode(n, dp, index + 1);
-        int b = mycode(n, dp, index + 2);
+        dp[n] = a+b ;
 
-        dp[index] = a + b;
-
-        return dp[index];
+        return dp[n] ;
     }
-
 }
